253380e934b154653334241a42b443cdc460d9d0
11 <19pw03@psgtech.ac.in>
2022-04-26 12:04:57 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        for (int i=0;i<5;i++) {
+            System.out.println(i);
+        }
+	// write your code here
+    }
+}
